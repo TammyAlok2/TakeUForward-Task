fb6eee75d340b254884fe511354a294becee8eee
Alok Tamrakar <109581769+TammyAlok2@users.noreply.github.com>
2024-03-21 23:10:36 +0530

    added some app data to index

--- Backend/index.js
+++ Backend/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { app } from "./src/app.js";
+import cors from 'cors'
 const port = 3000;
 import dotenv from "dotenv";
 import connectDB from "./src/db/index.js";
@@ -12,6 +13,33 @@ app.get("/", (req, res) => {
   res.json({message:'This is working fine'});
  
 });
+app.use(
+  cors({
+      origin:'*',
+      credentials:true
+  })
+)
+// when we use middleware
+app.use(
+  express.json({
+      limit:"16kb",
+  })
+)
+
+// to take the data from the url
+app.use(
+  express.urlencoded(
+      {
+          extended:true,
+          limit:"16kb"
+      }
+  )
+)
+
+app.use(express.static("public"))
+import userRouter from './src/Routes/Application.Routes.js'
+app.use('/api/v1',userRouter)
+
 
 connectDB()
   .then(() => {
